d4dbf6dfd80b307e0ad52f538df3d451c346a4c3
Felipe Augustos <felipe_augustos84@outlook.com.br>
2024-02-29 22:29:06 -0300

    feat: adding tanstack router

--- apps/ui/src/main.tsx
+++ apps/ui/src/main.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { ChakraProvider, ColorModeScript, ThemeConfig } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { FuelProvider } from '@fuels/react';
 import { defaultConnectors } from '@fuel-wallet/sdk'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { extendTheme } from '@chakra-ui/react'
+import { defaultTheme } from './theme/default.ts';
 
 const queryClient = new QueryClient();
 
-const config: ThemeConfig = {
-  initialColorMode: 'dark',
-}
-
-const theme = extendTheme({ config })
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>0
+    <ChakraProvider theme={defaultTheme}>
       <FuelProvider fuelConfig={{ connectors: defaultConnectors(), ui: true }}>
         <QueryClientProvider client={queryClient}>
           <ColorModeScript initialColorMode="dark" />
