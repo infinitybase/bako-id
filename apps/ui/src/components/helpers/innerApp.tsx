c0ced68dab860f9fbb8208e7a87a7de2ee2f926f
Felipe Augustos <felipe_augustos84@outlook.com.br>
2024-03-03 13:43:13 -0300

    feat: update innerApp to correct call

--- apps/ui/src/components/helpers/innerApp.tsx
+++ apps/ui/src/components/helpers/innerApp.tsx
@@ -1,11 +1,18 @@
-import { useFuelConnect } from '../../hooks';
 import { RouterProvider } from '@tanstack/react-router';
 import { router } from '../../routes';
+import { useIsConnected } from '@fuels/react';
 
 export function InnerApp() {
-  const { isConnected } = useFuelConnect()
+  const { isConnected} = useIsConnected();
+  console.debug(isConnected)
 
   if(isConnected === null) return;
+
+  if(!isConnected) {
+    // throw redirect({
+    //   to: '/connect',
+    // })
+  }
 
   return <RouterProvider router={router} context={{ isConnected }}/>
 }
