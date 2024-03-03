e8ec8a5eaf84870eb8d1e874c6a5f97b8880bd80
Felipe Augustos <felipe_augustos84@outlook.com.br>
2024-03-03 20:22:50 -0300

    chore: update header and connect button

--- apps/ui/src/components/header/index.tsx
+++ apps/ui/src/components/header/index.tsx
@@ -2,10 +2,10 @@ import { Connect } from '../helpers';
 import { Center, Image } from '@chakra-ui/react';
 
 export const Header = () => {
-  return <Center as="header" w="full" display="flex" justifyContent="space-between" alignItems="center" py={2} px={{ base: 4, md: 20, xl: 40 }} className="transition-all-05">
-
-        <Image src="/bakoID-logo.svg" width={190} height={75} alt="Bako logo" />
-
+  return (
+    <Center as="header" w="full" display="flex" justifyContent="space-between" alignItems="center" py={2} px={{ base: 4, md: 20, xl: 40 }} className="transition-all-05">
+      <Image src="/bakoID-logo.svg" width={190} height={75} alt="Bako logo" />
       <Connect />
     </Center>
+  )
 }
--- apps/ui/src/components/helpers/connect.tsx
+++ apps/ui/src/components/helpers/connect.tsx
@@ -5,7 +5,18 @@ export const Connect = () => {
   const { connect } = useConnectUI();
 
   return <Box>
-    <Button onClick={() => connect()} w="full" display="flex" gap={2} bgColor="button.500" fontSize="sm" color="black" mt={4} _hover={{ bgColor: 'button.600' }}>
+    <Button
+      onClick={() => connect()}
+      w="full"
+      display="flex"
+      gap={2}
+      bgColor="button.500"
+      fontSize="sm"
+      color="background.500"
+      mt={4}
+      _hover={{ bgColor: 'button.600' }}
+      className="transition-all-05"
+    >
       <Image src="/link.svg" alt="link" width={4} height={4} />
       Connect wallet
     </Button>
