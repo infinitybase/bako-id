86d60f20381ae975b1894f00c8b0d23a9e945edc
Felipe Augustos <felipe_augustos84@outlook.com.br>
2024-03-03 13:40:59 -0300

    feat: added new UI configs and tab name, with tab ico

---
+++ apps/ui/src/theme/breakpoints.ts
@@ -0,0 +1,8 @@
+export const breakpoints = {
+  base: "0px",
+  sm: "640px",
+  md: "768px",
+  lg: "1024px",
+  xl: "1280px",
+  "2xl": "1536px",
+};
--- apps/ui/src/theme/default.ts
+++ apps/ui/src/theme/default.ts
@@ -1,9 +1,11 @@
 import { extendTheme } from "@chakra-ui/react";
 import { colors } from "./colors";
+import { breakpoints } from './breakpoints.ts';
 
 const defaultTheme = extendTheme({
   initialColorMode: "dark",
   colors,
+  breakpoints
 });
 
 export { defaultTheme };
