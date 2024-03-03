4f3793a88957c4e71ab7ff949286a095e3c7bed6
Felipe Augustos <felipe_augustos84@outlook.com.br>
2024-03-03 13:41:43 -0300

    feat: refactor connect wallet button align to design

--- apps/ui/src/components/helpers/connect.tsx
+++ apps/ui/src/components/helpers/connect.tsx
@@ -1,12 +1,13 @@
-import { Button, Center } from '@chakra-ui/react';
+import { Box, Button, Image } from '@chakra-ui/react';
 import { useConnectUI } from '@fuels/react';
 
 export const Connect = () => {
   const { connect } = useConnectUI();
 
-  return <Center w="full" h="100vh" bgColor="#0b0c0c">
-    <Button onClick={() => connect()}>
-      Connect fuel wallet
+  return <Box>
+    <Button onClick={() => connect()} w="full" display="flex" gap={2} bgColor="button.500" fontSize="sm" color="black" mt={4} _hover={{ bgColor: 'button.600' }}>
+      <Image src="/link.svg" alt="link" width={4} height={4} />
+      Connect wallet
     </Button>
-  </Center>
+  </Box>
 }
