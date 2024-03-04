e8f5eae917e04855e1b098caf8a53480b2c09f80
Felipe Augustos <felipe_augustos84@outlook.com.br>
2024-03-04 12:35:48 -0300

    feat: added route to domain page

--- apps/ui/src/routes/childrens.ts
+++ apps/ui/src/routes/childrens.ts
@@ -1,39 +1,46 @@
-import { createRoute, redirect } from '@tanstack/react-router';
+import { createRoute } from '@tanstack/react-router';
 import { Home } from '../pages/home.tsx';
 import { rootRoute } from '../hooks/__root.ts';
 import { NotConnected } from '../components/auth/notConnected.tsx';
+import { Domain } from '../pages/domain.tsx';
 
 export const homeRoute = createRoute({
   getParentRoute: () => rootRoute,
   component: Home,
   path: '/',
-  shouldReload: true,
-  beforeLoad: ({ context }) => {
-    if (!context.isConnected) {
-      throw redirect({
-        to: '/auth',
-      })
-    }
-  }
+  // shouldReload: true,
+  // beforeLoad: ({ context }) => {
+  //   if (!context.isConnected) {
+  //     throw redirect({
+  //       to: '/auth',
+  //     })
+  //   }
+  // }
 })
 
 export const connectRoute = createRoute({
   getParentRoute: () => rootRoute,
   component: NotConnected,
   path: '/auth',
-  shouldReload: true,
-  loader: ({ context }) => {
-    if (context.isConnected) {
-      throw redirect({
-        to: '/',
-      })
-    }
-  },
-  beforeLoad: ({ context }) => {
-    if (context.isConnected) {
-      throw redirect({
-        to: '/',
-      })
-    }
-  }
+  // shouldReload: true,
+  // loader: ({ context }) => {
+  //   if (context.isConnected) {
+  //     throw redirect({
+  //       to: '/',
+  //     })
+  //   }
+  // },
+  // beforeLoad: ({ context }) => {
+  //   if (context.isConnected) {
+  //     throw redirect({
+  //       to: '/',
+  //     })
+  //   }
+  // }
+})
+
+export const domainRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  component: Domain,
+  path: '/$domain'
 })
--- apps/ui/src/routes/index.ts
+++ apps/ui/src/routes/index.ts
@@ -2,9 +2,9 @@ import {
   createRouter,
 } from '@tanstack/react-router';
 import { rootRoute } from '../hooks/__root.ts';
-import { connectRoute, homeRoute } from './childrens.ts';
+import { connectRoute, domainRoute, homeRoute } from './childrens.ts';
 
-const routeTree = rootRoute.addChildren([homeRoute, connectRoute])
+const routeTree = rootRoute.addChildren([homeRoute, connectRoute, domainRoute])
 
 export const router = createRouter({ routeTree, context: {
   isConnected: null
