6a9a820a27d21f85c3dd603aa909d4d942c884cd
Felipe Augustos <felipe_augustos84@outlook.com.br>
2024-03-04 12:35:30 -0300

    feat: removed not needed logic of wallet connect

--- apps/ui/src/components/layout/layout.tsx
+++ apps/ui/src/components/layout/layout.tsx
@@ -1,24 +1,24 @@
+import '../../theme/global.css'
 import { Center, Container } from '@chakra-ui/react';
-import { Outlet, useNavigate, useRouterState } from '@tanstack/react-router';
+import { Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import { BackgroundTexture } from '../helpers/backgroundTexture.tsx';
-import '../../theme/global.css'
 import { Header } from '../header';
-import { useIsConnected } from '@fuels/react';
+// import { useIsConnected } from '@fuels/react';
 
 const Layout = () => {
-  const { isConnected} = useIsConnected();
-  const routerState = useRouterState();
-  const path = routerState.location.pathname
-  const navigate = useNavigate()
-
-  if(!isConnected && path !== '/auth') {
-    navigate({ to: 'auth' }).then()
-  }
-
-  if(isConnected && path !== '/') {
-    navigate({ to: '/' }).then()
-  }
+  // const { isConnected} = useIsConnected();
+  // const routerState = useRouterState();
+  // const path = routerState.location.pathname
+  // const navigate = useNavigate()
+  //
+  // if(!isConnected && path !== '/auth') {
+  //   navigate({ to: 'auth' }).then()
+  // }
+  //
+  // if(isConnected && path !== '/') {
+  //   navigate({ to: '/' }).then()
+  // }
 
   return (
     <Container w="full" maxW="full" h="100vh" bgColor="background.500" display="flex" flexDir="column" padding={0} overflow="hidden">
