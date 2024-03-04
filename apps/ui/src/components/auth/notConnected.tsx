e556c339cae61f3d2dad89269ac23ca290dae2c5
Felipe Augustos <felipe_augustos84@outlook.com.br>
2024-03-04 00:06:55 -0300

    feat: created a notConnected feedback page

---
+++ apps/ui/src/components/auth/notConnected.tsx
@@ -0,0 +1,27 @@
+import { Center, Divider, Text, VStack } from '@chakra-ui/react';
+
+export const NotConnected = () => {
+  return (
+    <Center w="full" h="full" alignItems="center" zIndex={10}>
+      <VStack textAlign={"center"} spacing={6}>
+        <Text
+          bgGradient="linear(to-br, #FFC010, #B24F18)"
+          bgClip="text"
+          fontWeight={700}
+          fontSize={48}
+          lineHeight={1}
+          gap={2}
+        >
+          You're not connect to a wallet!
+        </Text>
+
+        <Text fontSize={15} color={"text.700"}>
+          You must connect to a wallet so we can start to setup your domains!
+        </Text>
+
+        <Divider w="60%" h="1px" border="none" bgGradient="linear(to-r, #FFC010, #B24F18)" />
+
+      </VStack>
+    </Center>
+  )
+}
