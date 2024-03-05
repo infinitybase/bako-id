dd3bcb424dac38a99560f4401a34c535dae33800
Felipe Augustos <felipe_augustos84@outlook.com.br>
2024-03-04 23:31:55 -0300

    feat: added go back to domain info page

--- apps/ui/src/pages/domain.tsx
+++ apps/ui/src/pages/domain.tsx
@@ -17,6 +17,7 @@ import {
   useToast, Fade
 } from '@chakra-ui/react';
 import { CopyIcon } from '@chakra-ui/icons';
+import { GoBack } from '../components/helpers';
 
 
 
@@ -59,81 +60,84 @@ const Domain = () => {
   }
 
   return (
-    <Center w="full" h="full" alignItems="center" zIndex={10} p={2}>
-      <Card background="background.600" w='full' maxW="2xl" boxShadow='lg'>
-        <CardBody>
-          <Flex alignItems="center" gap={6}>
-            <Box rounded={50} w="6rem" h="6rem" bgGradient="linear(to-br, #FFC010, #B24F18)"></Box>
-            {!data?.name ?
-              <Flex flexDir="column" gap={2}>
-                <Skeleton height="1rem" w="7rem" />
-                <Skeleton height="1rem" w="2rem" />
-              </Flex>
-              :
-              <Fade in={!!data?.name}>
+    <Center w="full" h="full" display="flex" flexDir="column" py={2} px={{ base: 4, md: 20, xl: 40 }} zIndex={10}>
+      <GoBack />
+      <Center w="full" h="full">
+        <Card background="background.600" w='full' maxW="2xl" boxShadow='lg'>
+          <CardBody>
+            <Flex alignItems="center" gap={6}>
+              <Box rounded={50} w="6rem" h="6rem" bgGradient="linear(to-br, #FFC010, #B24F18)"></Box>
+              {!data?.name ?
                 <Flex flexDir="column" gap={2}>
-                  <Heading size='md'>{data.name}</Heading>
-                  <Text fontSize='xs' color="grey.200">Fuel</Text>
+                  <Skeleton height="1rem" w="7rem" />
+                  <Skeleton height="1rem" w="2rem" />
                 </Flex>
-              </Fade>
-            }
-          </Flex>
-          <Stack mt='6' spacing='3' justifyContent="start">
-            <Box display="flex" flexDir="column" w="min-content" gap={2}>
-              <Text fontSize='sm' color="grey.200">Addresses</Text>
-              {!data?.resolver ?
-                <Skeleton height="2.5rem" w="7rem" rounded={8} /> :
-                <Fade in={!!data?.resolver}>
-                  <Button
-                    id="resolver"
-                    className="transition-all-05"
-                    variant="outiline"
-                    rounded={8}
-                    colorScheme="grey.200"
-                    p={2}
-                    borderColor="grey.300"
-                    borderWidth={1}
-                    background="semi-transparent"
-                    display="flex"
-                    gap={3}
-                    _hover={{ background: 'transparent', transform: 'translate(0px, -3px)' }}
-                    onClick={copy}
-                  >
-                    {shortString(data.resolver)}
-                    <CopyIcon />
-                  </Button>
+                :
+                <Fade in={!!data?.name}>
+                  <Flex flexDir="column" gap={2}>
+                    <Heading size='md'>{data.name}</Heading>
+                    <Text fontSize='xs' color="grey.200">Fuel</Text>
+                  </Flex>
                 </Fade>
               }
-            </Box>
-            <Box display="flex" flexDir="column" w="min-content" gap={2}>
-              <Text fontSize='sm' color="grey.200">Ownership</Text>
-              {!data?.owner ?
-                <Skeleton height="2.5rem" w="7rem" rounded={8} /> :
-                <Fade in={!!data?.owner}>
-                  <Button
-                    id="owmer"
-                    className="transition-all-05"
-                    variant="outiline"
-                    rounded={8}
-                    colorScheme="grey.200"
-                    p={2}
-                    borderColor="grey.300"
-                    borderWidth={1}
-                    background="semi-transparent"
-                    display="flex"
-                    gap={3}
-                    _hover={{ background: 'transparent', transform: 'translate(0px, -3px)' }}
-                    onClick={copy}
-                  >
-                    {shortString(data.owner)}
-                    <CopyIcon />
-                  </Button>
-                </Fade>
-              }
-            </Box>
-          </Stack>
-        </CardBody>
-      </Card>
+            </Flex>
+            <Stack mt='6' spacing='3' justifyContent="start">
+              <Box display="flex" flexDir="column" w="min-content" gap={2}>
+                <Text fontSize='sm' color="grey.200">Addresses</Text>
+                {!data?.resolver ?
+                  <Skeleton height="2.5rem" w="7rem" rounded={8} /> :
+                  <Fade in={!!data?.resolver}>
+                    <Button
+                      id="resolver"
+                      className="transition-all-05"
+                      variant="outiline"
+                      rounded={8}
+                      colorScheme="grey.200"
+                      p={2}
+                      borderColor="grey.300"
+                      borderWidth={1}
+                      background="semi-transparent"
+                      display="flex"
+                      gap={3}
+                      _hover={{ background: 'transparent', transform: 'translate(0px, -3px)' }}
+                      onClick={copy}
+                    >
+                      {shortString(data.resolver)}
+                      <CopyIcon />
+                    </Button>
+                  </Fade>
+                }
+              </Box>
+              <Box display="flex" flexDir="column" w="min-content" gap={2}>
+                <Text fontSize='sm' color="grey.200">Ownership</Text>
+                {!data?.owner ?
+                  <Skeleton height="2.5rem" w="7rem" rounded={8} /> :
+                  <Fade in={!!data?.owner}>
+                    <Button
+                      id="owmer"
+                      className="transition-all-05"
+                      variant="outiline"
+                      rounded={8}
+                      colorScheme="grey.200"
+                      p={2}
+                      borderColor="grey.300"
+                      borderWidth={1}
+                      background="semi-transparent"
+                      display="flex"
+                      gap={3}
+                      _hover={{ background: 'transparent', transform: 'translate(0px, -3px)' }}
+                      onClick={copy}
+                    >
+                      {shortString(data.owner)}
+                      <CopyIcon />
+                    </Button>
+                  </Fade>
+                }
+              </Box>
+            </Stack>
+          </CardBody>
+        </Card>
+      </Center>
     </Center>
   )
 }
