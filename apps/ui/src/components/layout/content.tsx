d3f9ca8b34f88492c7bd7cc9136f12c86fc4f276
Jon Ventura <jonathan_vmaced@hotmail.com>
2024-04-15 20:08:08 -0300

    feat(layout): adjusted layout to have sidebar

---
+++ apps/ui/src/components/layout/content.tsx
@@ -0,0 +1,27 @@
+import { Flex, FlexProps } from '@chakra-ui/react';
+
+export interface ContentProps extends FlexProps {}
+
+const Content = (props: ContentProps) => {
+  return (
+    <Flex
+      mt={0}
+      overflowY="scroll"
+      css={{ '&::-webkit-scrollbar': { width: '0' }, scrollbarWidth: 'none' }}
+      minH="calc(100vh - 82px)"
+      flex={1}
+      py={{
+        base: 3,
+        sm: 6,
+      }}
+      px={{
+        base: 5,
+        sm: 6,
+      }}
+    >
+      {props.children}
+    </Flex>
+  );
+};
+
+export { Content };
