37896b89af8537a7f909dc57af4d3f2b9f4e19d8
Jon Ventura <jonathan_vmaced@hotmail.com>
2024-04-16 14:53:06 -0300

    feat(header): added new header and user menu

--- apps/ui/src/components/header/index.tsx
+++ apps/ui/src/components/header/index.tsx
@@ -1,9 +1,19 @@
-import { Box, Center, Flex, Icon, Image, Skeleton } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  Icon,
+  Image,
+  Skeleton,
+} from '@chakra-ui/react';
 import { useIsConnected } from '@fuels/react';
 import { useNavigate } from '@tanstack/react-router';
 import { useFuelConnect } from '../../hooks';
 import { useScreenSize } from '../../hooks/useScreenSize';
+import { formatAddress } from '../../utils/formatter';
 import { Connect } from '../helpers';
+import { FileIcon } from '../icons/fileIcon';
 import { QuestionIcon } from '../icons/question';
 import { Info } from '../user';
 
@@ -18,7 +28,12 @@ export const Header = () => {
       return <Skeleton height="2.5rem" w="7rem" rounded={8} />;
     }
     if (wallet) {
-      return <Info name="user" account={wallet.address} />;
+      return (
+        <Info
+          name={formatAddress(wallet.address.toB256().toString())!}
+          account={wallet.address}
+        />
+      );
     }
     if (!isFetching && wallet === null)
       return (
@@ -48,19 +63,37 @@ export const Header = () => {
         <Image src="/bakoID-logo.svg" width={190} height={75} alt="Bako logo" />
       </button>
 
-      <Flex w="fit-content" align="center" justify="flex-end">
-        {account()}
-        {!isMobile && (
-          <Icon
+      <Flex w="fit-content" align="center" justify="flex-end" gap={2}>
+        <Flex w="full">
+          <Button
+            w="full"
+            bgColor="transparent"
             _hover={{
-              cursor: 'pointer',
-              opacity: 0.8,
+              bgColor: 'transparent',
+              color: 'button.500',
             }}
-            ml={4}
-            as={QuestionIcon}
-            w={6}
-            h={6}
-          />
+            color="grey.100"
+            fontWeight="normal"
+            fontSize="sm"
+            rightIcon={<FileIcon w={4} h={4} />}
+          >
+            My Handles
+          </Button>
+          {account()}
+        </Flex>
+        {!isMobile && (
+          <Box w="fit-content" ml={wallet ? 16 : 0}>
+            <Icon
+              _hover={{
+                cursor: 'pointer',
+                opacity: 0.8,
+              }}
+              ml={4}
+              as={QuestionIcon}
+              w={6}
+              h={6}
+            />
+          </Box>
         )}
       </Flex>
     </Center>
--- apps/ui/src/components/user/info.tsx
+++ apps/ui/src/components/user/info.tsx
@@ -3,16 +3,28 @@ import {
   Box,
   Button,
   Divider,
+  Flex,
+  Icon,
   Menu,
   MenuButton,
   MenuItem,
   MenuList,
+  Text,
 } from '@chakra-ui/react';
 import { useFuel } from '@fuels/react';
 import type { AbstractAddress } from 'fuels';
 import { useMemo, useState } from 'react';
+import { MdClose } from 'react-icons/md';
 import { formatAddress } from '../../utils/formatter.ts';
 import { RoundedUserIcon } from '../helpers/roundedUserIcon.tsx';
+import { BeginnersGuide } from '../icons/beginnersGuide.tsx';
+import { FileIcon } from '../icons/fileIcon.tsx';
+import { HowToSendCrypto } from '../icons/howToSendCrypto.tsx';
+import { LogoutIcon } from '../icons/logoutIcon.tsx';
+import { MiningCrypto } from '../icons/miningCrypto.tsx';
+import { MoreBako } from '../icons/moreBako.tsx';
+import { NotificationsIcon } from '../icons/notificationsIcon.tsx';
+import { useCustomToast } from '../toast/index.tsx';
 
 export const Info = ({
   name,
@@ -24,21 +36,26 @@ export const Info = ({
   const {
     fuel: { disconnect },
   } = useFuel();
+  const { successToast } = useCustomToast();
   const [hover, setHover] = useState(false);
 
   const copy = () => {
     navigator.clipboard.writeText(account.toString());
+    successToast({
+      title: 'Address copied to clipboard',
+    });
   };
 
   const icon = useMemo(() => {
-    return <RoundedUserIcon width="2em" heigth="2em" />;
+    return <RoundedUserIcon width="2.4em" heigth="2.4em" />;
   }, []);
 
   return (
     <Menu>
-      {({ isOpen }) => (
+      {({ isOpen, onClose }) => (
         <>
           <MenuButton
+            w="full"
             as={Button}
             variant="ghost"
             rounded="md"
@@ -49,52 +66,232 @@ export const Info = ({
             _active={{}}
             onMouseEnter={() => setHover(true)}
             onMouseLeave={() => setHover(false)}
-            p={0}
           >
             <Box
               display="flex"
-              gap={2}
+              gap={3}
               alignItems="center"
-              w="7rem"
+              justifyContent="flex-end"
+              w="15.5rem"
+              border="1px solid"
+              borderColor="stroke.500"
+              borderRadius="xl"
               color="white"
             >
-              {icon}
-              {name}
-
               {hover || isOpen ? (
                 <ChevronDownIcon
+                  position="relative"
                   className={`rotate-base ${isOpen ? undefined : 'rotate-div'}`}
                 />
               ) : null}
+
+              {name}
+              {icon}
             </Box>
           </MenuButton>
-          <MenuList background="background.600" minW="fit-content">
-            <Box
-              as="button"
-              w="full"
-              flex={0.5}
-              p={2}
-              fontSize="sm"
-              display="flex"
-              alignItems="center"
-              gap={2}
-              cursor="pointer"
-              onClick={copy}
-              color="white"
-            >
-              {formatAddress(account.toString())}
-              <CopyIcon />
-            </Box>
-            <Divider w="90%" h="0px" bgColor="#FFF" m="auto" />
+          <MenuList
+            background="background.900"
+            maxW={['350px', '400px']}
+            w="auto"
+            position="relative"
+            right={['unset', 'unset', 'unset', -16]}
+            display="flex"
+            p={2}
+            flexDirection="column"
+            borderColor="stroke.500"
+          >
+            {/* User */}
             <MenuItem
-              onClick={() => disconnect()}
-              p={2}
-              background="transparent"
-              color="grey.300"
-              fontSize="sm"
+              bgColor="transparent"
+              _hover={{
+                cursor: 'default',
+              }}
+              display="flex"
+              gap={4}
             >
-              Disconnect
+              <Flex
+                h="full"
+                w="full"
+                alignItems="flex-start"
+                justifyContent="space-between"
+              >
+                <Flex gap={3}>
+                  <RoundedUserIcon width="3em" heigth="3em" />
+                  <Flex display="flex" flexDirection="column" gap={1}>
+                    <Text
+                      display="flex"
+                      alignItems="center"
+                      gap={4}
+                      fontWeight="bold"
+                    >
+                      {formatAddress(account.toString())}
+                      <CopyIcon
+                        onClick={copy}
+                        _hover={{
+                          cursor: 'pointer',
+                          color: 'button.500',
+                        }}
+                      />
+                    </Text>
+                    <Text fontSize="sm" color="section.500">
+                      Your current logged in address
+                    </Text>
+                  </Flex>
+                </Flex>
+                <Icon
+                  w={5}
+                  h={5}
+                  as={MdClose}
+                  onClick={() => onClose()}
+                  _hover={{
+                    cursor: 'pointer',
+                    opacity: 0.8,
+                  }}
+                />
+              </Flex>
             </MenuItem>
+            <Divider w="95%" color="grey.300" mx="auto" mt={3} />
+
+            {/* My Handles */}
+            <MenuItem
+              bgColor="transparent"
+              color="grey.200"
+              my={2}
+              gap={2}
+              onClick={() => {}}
+              _hover={{
+                cursor: 'pointer',
+                color: 'button.500',
+                opacity: 0.9,
+              }}
+            >
+              <Icon as={FileIcon} />
+              My Handles
+            </MenuItem>
+            <Divider w="95%" color="grey.300" mx="auto" />
+
+            {/* Notifications */}
+            <MenuItem
+              bgColor="transparent"
+              color="grey.200"
+              my={2}
+              gap={2}
+              onClick={() => {}}
+              _hover={{
+                cursor: 'pointer',
+                color: 'button.500',
+                opacity: 0.9,
+              }}
+            >
+              <Icon as={NotificationsIcon} />
+              Notifications
+            </MenuItem>
+            <Divider w="95%" color="grey.300" mx="auto" />
+
+            {/* Logout */}
+            <MenuItem
+              bgColor="transparent"
+              color="grey.200"
+              my={2}
+              gap={2}
+              onClick={disconnect}
+              _hover={{
+                cursor: 'pointer',
+                color: 'button.500',
+                opacity: 0.9,
+              }}
+            >
+              <Icon as={LogoutIcon} />
+              Logout
+            </MenuItem>
+            <Divider w="95%" color="grey.300" mx="auto" />
+
+            <Flex
+              direction="column"
+              mx={3}
+              mt={3}
+              alignItems="flex-start"
+              gap={4}
+              maxW="full"
+            >
+              <Text color="grey.200" fontSize="xs">
+                More from Bako
+              </Text>
+              <Flex
+                w="full"
+                maxW="full"
+                overflowX="scroll"
+                onWheel={(e) => {
+                  e.preventDefault();
+                  e.currentTarget.scrollLeft += e.deltaY;
+                }}
+                scrollBehavior="smooth"
+                css={{
+                  '&::-webkit-scrollbar': {
+                    width: 0,
+                    background: 'transparent',
+                  },
+                }}
+              >
+                <Box
+                  w="130px"
+                  h="130px"
+                  rounded="xl"
+                  _hover={{
+                    cursor: 'pointer',
+                    opacity: 0.8,
+                  }}
+                  onClick={() =>
+                    window.open('https://www.bako.global', '_blank')
+                  }
+                  as={MoreBako}
+                />
+                <Box
+                  w="130px"
+                  h="130px"
+                  rounded="xl"
+                  _hover={{
+                    cursor: 'pointer',
+                    opacity: 0.8,
+                  }}
+                  as={BeginnersGuide}
+                />
+                <Box
+                  w="130px"
+                  h="130px"
+                  rounded="xl"
+                  _hover={{
+                    cursor: 'pointer',
+                    opacity: 0.8,
+                  }}
+                  as={MiningCrypto}
+                />
+                <Box
+                  w="130px"
+                  h="130px"
+                  rounded="xl"
+                  _hover={{
+                    cursor: 'pointer',
+                    opacity: 0.8,
+                  }}
+                  as={HowToSendCrypto}
+                />
+              </Flex>
+            </Flex>
+
+            <Divider w="95%" color="grey.300" mx="auto" mb={3} />
+
+            <Flex
+              color="grey.100"
+              fontSize="xs"
+              alignItems="center"
+              justifyContent="center"
+              gap={4}
+            >
+              <Text>Privacy policy</Text>
+              <Box bgColor="grey.100" w={1} h={1} rounded="full" />
+              <Text>Terms of use</Text>
+            </Flex>
           </MenuList>
         </>
       )}
