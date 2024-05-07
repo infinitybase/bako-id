f4a2ae409f0b479e8fa163df17949637c147764c
Jon Ventura <jonathan_vmaced@hotmail.com>
2024-05-07 15:33:18 -0300

    feat(theme): added input theme and form theme

---
+++ apps/ui/src/theme/components/input.ts
@@ -0,0 +1,67 @@
+import { defineStyle, defineStyleConfig } from '@chakra-ui/react';
+
+const inputActiveStyle = {
+  borderColor: 'section.200',
+  color: 'white',
+  bg: 'dark.250',
+  boxShadow:
+    // '0 0 0 3px color-mix(in srgb, var(--chakra-colors-brand-500)) 70%, transparent)',
+    '0 0 0 3px color-mix(in srgb, var(--chakra-colors-section-200) 50%, transparent)',
+  '~ .chakra-input__right-element': {
+    background: 'dark.250',
+  },
+};
+
+const inputInvalidStyle = {
+  borderColor: 'error.500',
+  bg: 'dark.250',
+  boxShadow: 'error.600',
+  '~ .chakra-input__right-element': {
+    background: 'dark.250',
+  },
+};
+
+const baseStyle = defineStyle({
+  field: {
+    bg: 'dark.250',
+    color: 'grey.200',
+    fontSize: 'md',
+    borderColor: 'grey.600',
+    boxShadow: 'none',
+    borderWidth: 1,
+    pt: 5,
+    pb: 1,
+    px: 5,
+    height: 'auto',
+    borderRadius: 10,
+    _hover: {
+      borderColor: 'dark.100',
+    },
+    _blur: {
+      background: 'dark.250',
+    },
+    _invalid: inputInvalidStyle,
+    _focus: inputActiveStyle,
+    _focusVisible: inputActiveStyle,
+    _placeholder: {
+      color: 'grey.200',
+      fontWeight: 'medium',
+    },
+  },
+  addon: {},
+  element: {},
+});
+
+const Input = defineStyleConfig({
+  baseStyle,
+  defaultProps: {
+    colorScheme: 'grey',
+  },
+  variants: {
+    custom: baseStyle,
+  },
+});
+
+const Textarea = Input;
+
+export { Input, Textarea, baseStyle as inputStyle };
