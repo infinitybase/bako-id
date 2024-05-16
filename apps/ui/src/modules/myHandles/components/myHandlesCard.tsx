2ac8e9892341a20d6a13f7b9bb45c713bdd1d852
Jon Ventura <jonathan_vmaced@hotmail.com>
2024-05-16 09:10:28 -0300

    fix(myhandles): adjusted my handles design

--- apps/ui/src/modules/myHandles/components/myHandlesCard.tsx
+++ apps/ui/src/modules/myHandles/components/myHandlesCard.tsx
@@ -17,7 +17,7 @@ interface MyHandlesCard {
 
 export const MyHandlesCard = ({ handles }: MyHandlesCard) => {
   return (
-    <Center w="full" h={['auto', '70%', '4xl', '3xl']} alignItems="center">
+    <Center w="full" h={['auto', '70%', 'xl', '3xl']} alignItems="center">
       <Card
         border="1px solid"
         borderColor="stroke.500"
