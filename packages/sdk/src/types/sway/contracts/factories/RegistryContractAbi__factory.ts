57112a2589a56b102093c4e4b1c421edf99c3964
Jon Ventura <jonathan_vmaced@hotmail.com>
2024-06-04 19:07:13 -0300

    feat(sdk): added new contract deployment

--- packages/sdk/src/types/sway/contracts/factories/RegistryContractAbi__factory.ts
+++ packages/sdk/src/types/sway/contracts/factories/RegistryContractAbi__factory.ts
@@ -357,15 +357,15 @@ const _abi = {
       },
       "attributes": [
         {
+          "name": "payable",
+          "arguments": []
+        },
+        {
           "name": "storage",
           "arguments": [
             "read",
             "write"
           ]
-        },
-        {
-          "name": "payable",
-          "arguments": []
         }
       ]
     },
