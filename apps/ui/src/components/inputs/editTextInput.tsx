2f9ece5d9a1374dfd97ebc10184d570e299f8eed
Jon Ventura <jonathan_vmaced@hotmail.com>
2024-08-06 09:08:54 -0300

    fix(input): adjusted input for requests

--- apps/ui/src/components/inputs/editTextInput.tsx
+++ apps/ui/src/components/inputs/editTextInput.tsx
@@ -21,7 +21,7 @@ import { TrashIcon } from '../icons/trashIcon';
 interface IEditTextValueInput extends InputProps {
   title: string;
   modalType: string;
-  onSave: (metadata: Metadata) => Promise<void>;
+  onSave: (metadata: Metadata) => void;
   onClose: () => void;
 }
 
@@ -45,15 +45,10 @@ export const EditTextValueInput = (props: IEditTextValueInput) => {
   });
 
   const handleSave = () => {
-    props
-      .onSave({
-        key: props.modalType,
-        value: inputValue,
-      })
-      .then(
-        () => props.onClose(),
-        (error) => console.error(error),
-      );
+    props.onSave({
+      key: props.modalType,
+      value: inputValue,
+    });
     setInputValue('');
   };
 
