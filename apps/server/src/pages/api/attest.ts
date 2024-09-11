6de0bd49a1acd6fdb53330c499ab7c4330ad6bb3
luisburigo <guga.burigo29@outlook.com>
2024-09-10 21:52:23 -0300

    fix(attestation): get provider by url

--- apps/server/src/pages/api/attest.ts
+++ apps/server/src/pages/api/attest.ts
@@ -1,37 +1,38 @@
-import { attest, config } from '@bako-id/sdk';
+import { attest } from '@bako-id/sdk';
 import { Provider, Wallet } from 'fuels';
 import type { NextApiRequest, NextApiResponse } from 'next';
-const { API_PRIVATE_KEY } = process.env;
+
+const { API_PRIVATE_KEY, PROVIDER_URL } = process.env;
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse,
+  res: NextApiResponse
 ) {
-  if (req.method !== 'POST') {
+  const { method, body } = req;
+
+  if (method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
 
-  if (req.body) {
-    try {
-      const provider = await Provider.create(config.PROVIDER_DEPLOYED!);
-      const attester = Wallet.fromPrivateKey(API_PRIVATE_KEY!, provider);
+  try {
+    const provider = await Provider.create(PROVIDER_URL!);
+    const attester = Wallet.fromPrivateKey(API_PRIVATE_KEY!, provider);
 
-      const attestationHash = await attest({
-        attester,
-        data: {
-          id: req.body.id,
-          app: req.body.app,
-          handle: req.body.handle,
-        },
-      });
+    const attestationHash = await attest({
+      attester,
+      data: {
+        id: body.id,
+        app: body.app,
+        handle: body.handle,
+      },
+    });
 
-      return res.json({
-        data: attestationHash,
-      });
-    } catch (error) {
-      return res.json({
-        message: error,
-      });
-    }
+    return res.json({
+      data: attestationHash,
+    });
+  } catch (error) {
+    return res.json({
+      message: error,
+    });
   }
 }
