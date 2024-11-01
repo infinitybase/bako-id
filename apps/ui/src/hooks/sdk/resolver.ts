e3a15da2bc9d06cc519e3e40ab1888914b4c25db
luisburigo <guga.burigo29@outlook.com>
2024-10-31 21:36:02 -0300

    fix: search domain

--- apps/ui/src/hooks/sdk/resolver.ts
+++ apps/ui/src/hooks/sdk/resolver.ts
@@ -1,14 +1,25 @@
 import { ResolverContract } from '@bako-id/sdk';
 import { useProvider } from '@fuels/react';
+import { useQuery } from '@tanstack/react-query';
+import { Provider } from 'fuels';
 import { useMemo } from 'react';
 
 export const useResolverContract = () => {
   const { provider } = useProvider();
 
+  const providerQuery = useQuery({
+    queryKey: ['resolverProvider', provider],
+    queryFn: async () => {
+      if (provider) return provider;
+      return Provider.create(import.meta.env.VITE_PROVIDER_URL);
+    },
+  });
+
   const contract = useMemo(() => {
-    if (!provider) return null;
-    return ResolverContract.create(provider);
-  }, [provider]);
+    console.log('provider', providerQuery.data);
+    if (!providerQuery.data) return null;
+    return ResolverContract.create(providerQuery.data);
+  }, [providerQuery.data]);
 
   return contract;
 };
