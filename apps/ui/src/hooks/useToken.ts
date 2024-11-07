56a362643a4d5521b92bc872df262832cf67c9e5
luisburigo <guga.burigo29@outlook.com>
2024-11-07 13:16:23 -0300

    fix(sdk): simulate handle and get token data without an account connected

--- apps/ui/src/hooks/useToken.ts
+++ apps/ui/src/hooks/useToken.ts
@@ -1,14 +1,34 @@
+import { RegistryContract } from '@bako-id/sdk';
+import { useProvider } from '@fuels/react';
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from '@tanstack/react-router';
-import { useRegistryContract } from './sdk';
+import { Provider } from 'fuels';
 
 export const useToken = () => {
   const { domain } = useParams({ strict: false });
-  const registryContract = useRegistryContract();
+  const { provider } = useProvider();
 
   const { data } = useQuery({
     queryKey: ['token', domain],
-    queryFn: async () => registryContract?.token(domain),
+    queryFn: async () => {
+      try {
+        let registryContract: RegistryContract;
+
+        if (provider) {
+          registryContract = RegistryContract.create(provider);
+        } else {
+          const provider = await Provider.create(
+            import.meta.env.VITE_PROVIDER_URL
+          );
+          registryContract = RegistryContract.create(provider);
+        }
+
+        return registryContract.token(domain);
+      } catch (e) {
+        console.log(e);
+        throw e;
+      }
+    },
     enabled: !!domain,
   });
 
