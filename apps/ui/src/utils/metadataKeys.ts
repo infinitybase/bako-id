a304ced62e6f0e9e28fc3ff45f9a67b3acc3a4a6
Pedro Barão <phbarao@gmail.com>
2024-11-08 15:29:34 -0300

    refactor: remove commented code

--- apps/ui/src/utils/metadataKeys.ts
+++ apps/ui/src/utils/metadataKeys.ts
@@ -1,22 +1,3 @@
-// export enum SocialMetadataKeys {
-//   Twitter = 'social:x',
-//   Farcaster = 'social:farcaster',
-//   Github = 'social:github',
-//   Telegram = 'social:telegram',
-//   Discord = 'social:discord',
-//   Mail = 'social:email',
-// }
-
-// export enum ContactMetadataKeys {
-//   Nickname = 'contact:nickname',
-//   Bio = 'contact:bio',
-//   Location = 'contact:location',
-// }
-
-// export enum LinkMetadataKeys {
-//   Website = 'link:website',
-// }
-
 export enum MetadataKeys {
   SOCIAL_DISCORD = 'social:discord',
   SOCIAL_FACEBOOK = 'social:facebook',
