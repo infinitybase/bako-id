dd377acf74ef07eb440844b326748978a0c8f097
luisburigo <guga.burigo29@outlook.com>
2024-11-12 14:35:05 -0300

    feat(contracts): save json file with all contracts ids on deploy

--- packages/contracts/fuels.config.ts
+++ packages/contracts/fuels.config.ts
@@ -1,5 +1,8 @@
+import * as fs from 'node:fs';
+import path from 'node:path';
 import dotenv from 'dotenv';
 import { createConfig } from 'fuels';
+import { resolveNetwork } from './src';
 
 dotenv.config();
 
@@ -10,4 +13,30 @@ export default createConfig({
   output: './src/artifacts',
   privateKey: process.env.PRIVATE_KEY,
   providerUrl: process.env.PROVIDER_URL,
+  onDeploy: async (config, data) => {
+    const { providerUrl, output } = config;
+    const networkName = resolveNetwork(providerUrl);
+
+    const contractIdJsonPath = path.join(output, 'contracts-fuel.json');
+
+    if (!fs.existsSync(contractIdJsonPath)) {
+      fs.writeFileSync(contractIdJsonPath, '{}');
+    }
+
+    const contractsIdsFile = JSON.parse(
+      fs.readFileSync(contractIdJsonPath, 'utf-8')
+    );
+    const contractsIds = Object.fromEntries(
+      data.map(({ name, contractId }) => [name, contractId])
+    );
+
+    const networkContractsIds = {
+      ...contractsIdsFile,
+      [networkName]: contractsIds,
+    };
+    fs.writeFileSync(
+      contractIdJsonPath,
+      JSON.stringify(networkContractsIds, null, 2)
+    );
+  },
 });
