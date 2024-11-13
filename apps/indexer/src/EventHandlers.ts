4a57192e7b7208610bf0478f0f7998ba2610ed71
luisburigo <guga.burigo29@outlook.com>
2024-11-12 23:12:49 -0300

    feat(indexer): add envio indexer

--- apps/indexer/src/EventHandlers.ts
+++ apps/indexer/src/EventHandlers.ts
@@ -1,18 +1,13 @@
-/*
- * Please refer to https://docs.envio.dev for a thorough guide on all Envio indexer features
- */
-import {
-    Manager,
-} from "generated";
+import { Manager } from 'generated';
 
-Manager.ManagerLogEvent.handler(async ({event, context}) => {
-    context.Manager_ManagerLogEvent.set({
-        id: `${event.params.name_hash}-${event.params.owner.payload.bits}`,
-        name: event.params.name,
-        name_hash: event.params.name_hash,
-        owner: event.params.owner.payload.bits,
-        period: event.params.period,
-        resolver: event.params.resolver.payload.bits,
-        timestamp: String(event.params.timestamp),
-    });
+Manager.ManagerLogEvent.handler(async ({ event, context }) => {
+  context.Manager_ManagerLogEvent.set({
+    id: `${event.params.name_hash}-${event.params.owner.payload.bits}`,
+    name: event.params.name,
+    name_hash: event.params.name_hash,
+    owner: event.params.owner.payload.bits,
+    period: event.params.period,
+    resolver: event.params.resolver.payload.bits,
+    timestamp: String(event.params.timestamp),
+  });
 });
