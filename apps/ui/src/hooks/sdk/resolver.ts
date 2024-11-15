7fbe57cdf2a9271ecd4b2bfa46636cac31b927f7
luisburigo <guga.burigo29@outlook.com>
2024-11-15 15:49:24 -0300

    fix(ui): use correct provider when connect in contract

--- apps/ui/src/hooks/sdk/resolver.ts
+++ apps/ui/src/hooks/sdk/resolver.ts
@@ -1,14 +1,26 @@
 import { ResolverContract } from '@bako-id/sdk';
+import { useProvider } from '@fuels/react';
+import { useQuery } from '@tanstack/react-query';
+import { Provider } from 'fuels';
 import { useMemo } from 'react';
-import { useProvider } from '../fuel';
+
+const { VITE_PROVIDER_URL } = import.meta.env;
 
 export const useResolverContract = () => {
-  const provider = useProvider();
+  const { provider } = useProvider();
+
+  const providerQuery = useQuery({
+    queryKey: ['resolverProvider', provider?.url ?? VITE_PROVIDER_URL],
+    queryFn: async () => {
+      return provider || Provider.create(VITE_PROVIDER_URL);
+    },
+    staleTime: Number.POSITIVE_INFINITY,
+  });
 
   const contract = useMemo(() => {
-    if (!provider) return null;
-    return ResolverContract.create(provider);
-  }, [provider]);
+    if (!providerQuery.data) return null;
+    return ResolverContract.create(providerQuery.data);
+  }, [providerQuery.data]);
 
   return contract;
 };
