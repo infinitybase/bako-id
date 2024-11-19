ed4cf6ae151a4f2ee270328bc23ca2bc0a8c0268
Pedro Barão <phbarao@gmail.com>
2024-11-19 15:01:21 -0300

    feaet: add ens cta

--- apps/ui/src/components/sidebar/profile.tsx
+++ apps/ui/src/components/sidebar/profile.tsx
@@ -4,6 +4,7 @@ import {
   Flex,
   FormControl,
   Text,
+  useDisclosure,
   VStack,
   type BoxProps,
 } from '@chakra-ui/react';
@@ -17,90 +18,102 @@ import { BakoSafeLogo } from '../icons/bakoSafeLogo';
 import { UserIcon } from '../icons/userIcon';
 import { DropdownAutocomplete } from '../inputs/dropdownAutocomplete';
 import { useSidebar } from './hooks/useSidebar';
+import { ENSBanner } from '../buttons/ensBanner';
+import { NSDialog } from '../../modules/ens/components/dialog';
 
 interface ProfileSidebarProps extends BoxProps {}
 
 const ProfileSidebar = (props: ProfileSidebarProps) => {
   const { isMyDomain } = useSidebar();
   const [active, setActive] = useState<string>('');
+  const ensDialogState = useDisclosure();
 
   return (
-    <Box
-      w="100%"
-      maxW="350px"
-      blur="30px"
-      bgColor="gradient.100"
-      p={5}
-      py="auto"
-      display="flex"
-      flexDir="column"
-      justifyContent="space-between"
-      {...props}
-    >
-      <VStack mt={4}>
-        <>
-          <Box w="100%" mb={6}>
-            <FormControl
-              display="flex"
-              flexDir="column"
-              alignItems="flex-start"
-              justifyContent="center"
-              gap={4}
-            >
-              <Text color="section.200" fontSize="md" fontWeight="semibold">
-                More Handles?
-              </Text>
-              <DropdownAutocomplete
-                bgColor="#201F1D"
-                key="profile-sidebar-autocomplete"
-              />
-            </FormControl>
-          </Box>
-          <Divider borderColor="grey.600" mb={6} w="full" />
-        </>
+    <>
+      <NSDialog
+        isOpen={ensDialogState.isOpen}
+        onClose={ensDialogState.onClose}
+      />
 
-        <SidebarButton
-          setActive={setActive}
-          active={active}
-          title="Profile"
-          icon={<UserIcon w={5} h={5} />}
-        />
-        {/* <SidebarButton
+      <Box
+        w="100%"
+        maxW="350px"
+        blur="30px"
+        bgColor="gradient.100"
+        p={5}
+        py="auto"
+        display="flex"
+        flexDir="column"
+        justifyContent="space-between"
+        {...props}
+      >
+        <VStack mt={4}>
+          <>
+            <Box w="100%" mb={6}>
+              <FormControl
+                display="flex"
+                flexDir="column"
+                alignItems="flex-start"
+                justifyContent="center"
+                gap={4}
+              >
+                <Text color="section.200" fontSize="md" fontWeight="semibold">
+                  More Handles?
+                </Text>
+                <DropdownAutocomplete
+                  bgColor="#201F1D"
+                  key="profile-sidebar-autocomplete"
+                />
+              </FormControl>
+            </Box>
+            <Divider borderColor="grey.600" mb={6} w="full" />
+          </>
+
+          <SidebarButton
+            setActive={setActive}
+            active={active}
+            title="Profile"
+            icon={<UserIcon w={5} h={5} />}
+          />
+          {/* <SidebarButton
           setActive={setActive}
           active={active}
           title="Ownership"
           icon={<KeyIcon w={5} h={5} />}
         /> */}
-        <SidebarButton
-          setActive={setActive}
-          active={active}
-          title="More"
-          icon={<BsThreeDotsVertical />}
-        />
-      </VStack>
-      <VStack spacing={4}>
-        {isMyDomain && (
-          <Flex w="full" flexDirection="column" gap={2}>
-            {/* <SidebarBanner
+          <SidebarButton
+            setActive={setActive}
+            active={active}
+            title="More"
+            icon={<BsThreeDotsVertical />}
+          />
+        </VStack>
+        <VStack spacing={4}>
+          {isMyDomain && (
+            <Flex w="full" flexDirection="column" gap={2}>
+              {/* <SidebarBanner
               text="Click here to verify your X account and get benefits"
               icon={TwitterBannerIcon}
             /> */}
 
-            {/* <SidebarBanner
+              {/* <SidebarBanner
               text="Click here to verify your Farcaster account and get benefits"
               icon={FarcasterIcon}
               bgColor="#7F5FC7"
               iconColor="white"
             /> */}
-          </Flex>
-        )}
+            </Flex>
+          )}
 
-        <BakoSafeBanner
-          onClick={() => window.open('https://www.bako.global', '_blank')}
-          icon={BakoSafeLogo}
-        />
-      </VStack>
-    </Box>
+          <ENSBanner onClick={ensDialogState.onOpen} />
+
+          <BakoSafeBanner
+            onClick={() => window.open('https://www.bako.global', '_blank')}
+            icon={BakoSafeLogo}
+          />
+        </VStack>
+      </Box>
+    </>
   );
 };
 
