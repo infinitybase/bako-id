63fad1b4ae1195101cbcaad51517d0846edd15d4
luisburigo <guga.burigo29@outlook.com>
2024-12-02 22:36:55 -0300

    feat(ui): show all nft's of handle resolver

--- apps/server/app/[network]/addr/[name]/route.ts
+++ apps/server/app/[network]/addr/[name]/route.ts
@@ -1,16 +1,6 @@
-import { FILENAME, getJsonFile } from '@/s3';
-import { validateNetwork } from '@/utils';
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { type NextRequest, NextResponse } from 'next/server';
-
-export const getResolver = async (name: string, network: string) => {
-  const { chainId } = validateNetwork(network);
-  const resovlerFileName = `${chainId}/${FILENAME}`;
-  const offChainData = await getJsonFile(resovlerFileName);
-  const address =
-    offChainData.resolversName[name.toString().replace('@', '') as string];
-  return address;
-};
+import { getResolver } from './resolver';
 
 export async function GET(req: NextRequest) {
   const [network, , name] = req.nextUrl.pathname.split('/').filter((a) => !!a);
