f0f737ed73954e5f98f1f98b9eda779e36be7079
vitor <pereiravitor1218@gmail.com>
2025-04-25 15:36:47 -0300

    feat(nft): implement NftSearchAssetModal for asset selection with search functionality

---
+++ apps/ui/src/modules/profile/components/nft/NftSearchAssetModal.tsx
@@ -0,0 +1,182 @@
+import UnknownAsset from '@/assets/unknown-asset.png';
+import { Dialog } from '@/components';
+import { useListAssets } from '@/hooks/marketplace/useListAssets';
+import { CloseIcon } from '@chakra-ui/icons';
+import {
+  CircularProgress,
+  Divider,
+  Flex,
+  FormControl,
+  FormLabel,
+  Image,
+  Input,
+  List,
+  ListItem,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
+import { useWallet } from '@fuels/react';
+import { useQuery } from '@tanstack/react-query';
+import { bn } from 'fuels';
+import { useMemo, useState } from 'react';
+
+interface NftSearchAssetModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onSelect: (asset: { id: string; name?: string; icon?: string }) => void;
+}
+
+export const NftSearchAssetModal = ({
+  isOpen,
+  onClose,
+  onSelect,
+}: NftSearchAssetModalProps) => {
+  const [search, setSearch] = useState('');
+  const { wallet } = useWallet();
+  const { assets, isLoading: isAssetsLoading } = useListAssets();
+
+  const { data, isLoading: isBalancesLoading } = useQuery({
+    queryKey: ['balances'],
+    queryFn: async () => {
+      if (wallet) {
+        const { balances } = await wallet.getBalances();
+
+        return assets.map((asset) => {
+          const balance = balances.find(
+            (balance) => balance.assetId === asset.id
+          );
+          return {
+            ...asset,
+            balance: balance ? bn(balance.amount) : bn(0),
+          };
+        });
+      }
+    },
+  });
+  const isLoading = isAssetsLoading || isBalancesLoading;
+
+  const filteredAssets = useMemo(() => {
+    return (
+      data?.filter((asset) => {
+        const name = asset.metadata?.name?.toLowerCase() ?? '';
+        const symbol = asset.metadata?.symbol?.toLowerCase() ?? '';
+        const searchLower = search.toLowerCase();
+
+        return (
+          name.includes(searchLower) ||
+          symbol.includes(searchLower) ||
+          asset.id.toLowerCase().includes(searchLower)
+        );
+      }) ?? []
+    );
+  }, [data, search]);
+
+  const isEmpty = useMemo(() => filteredAssets.length === 0, [filteredAssets]);
+
+  const handleSelect = (asset: {
+    id: string;
+    name?: string;
+    icon?: string;
+  }) => {
+    onSelect(asset);
+    onClose();
+  };
+
+  return (
+    <Dialog.Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      modalTitle="Assets"
+      modalSubtitle="Send single or batch payments with multi assets. You can send multiple types of assets to different addresses."
+    >
+      <Stack gap={6} w="full">
+        <FormControl position="relative">
+          <CloseIcon
+            position="absolute"
+            right={3}
+            top="50%"
+            transform="translateY(-50%)"
+            fontSize="sm"
+            zIndex={99}
+          />
+          <Input
+            size="lg"
+            placeholder=" "
+            variant="outlined"
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+          />
+          <FormLabel fontSize="sm">Search asset</FormLabel>
+        </FormControl>
+
+        <Divider />
+
+        <List spacing={2}>
+          {!isLoading &&
+            filteredAssets.map((asset) => (
+              <ListItem
+                key={asset.id}
+                display="flex"
+                alignItems="center"
+                justifyContent="space-between"
+                p={3}
+                borderWidth={1}
+                borderColor="grey.600"
+                borderRadius="lg"
+                borderStyle="solid"
+                cursor="pointer"
+                transition="all 0.2s"
+                _hover={{
+                  borderColor: 'section.200',
+                }}
+                onClick={() =>
+                  handleSelect({
+                    id: asset.id,
+                    name:
+                      asset.metadata?.name ??
+                      asset.metadata?.symbol ??
+                      'Unknown Asset',
+                    icon: asset.metadata?.icon ?? UnknownAsset,
+                  })
+                }
+              >
+                <Flex alignItems="center">
+                  <Image
+                    src={asset.metadata?.icon ?? UnknownAsset}
+                    alt={asset.metadata?.name ?? 'Unknown Asset'}
+                    boxSize="24px"
+                    borderRadius="full"
+                    mr={2}
+                  />
+                  <Text>{asset.metadata?.name}</Text>
+                </Flex>
+
+                <Stack spacing={1} direction="row" alignItems="center">
+                  <Text>
+                    {Number(
+                      asset.balance?.formatUnits(asset.metadata?.decimals)
+                    )}
+                  </Text>
+                  <Text textTransform="uppercase">
+                    {asset.metadata?.symbol}
+                  </Text>
+                </Stack>
+              </ListItem>
+            ))}
+
+          {!isLoading && isEmpty && (
+            <ListItem>
+              <Text>No assets found</Text>
+            </ListItem>
+          )}
+
+          {isLoading && (
+            <ListItem>
+              <CircularProgress isIndeterminate size="24px" />
+            </ListItem>
+          )}
+        </List>
+      </Stack>
+    </Dialog.Modal>
+  );
+};
