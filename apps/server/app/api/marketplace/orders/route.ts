55778cded65ee3f6c85e4affe7fbf4c88f646980
vitor <pereiravitor1218@gmail.com>
2025-04-30 14:47:53 -0300

    feat(api): implement GET endpoint for fetching marketplace orders

---
+++ apps/server/app/api/marketplace/orders/route.ts
@@ -0,0 +1,31 @@
+import { marketplaceClient } from '@/services/marketplace';
+import { OrderStatus } from '@/types/marketplace';
+
+export async function GET(request: Request) {
+  try {
+    const url = new URL(request.url);
+    const page = Number(url.searchParams.get('page')) || 1;
+    const limit = Number(url.searchParams.get('limit')) || 12;
+    const id = url.searchParams.get('id');
+
+    const where = {
+      status: {
+        _eq: OrderStatus.CREATED,
+      },
+      ...(id && { id: { _eq: id } }),
+    };
+
+    const total = await marketplaceClient.getOrdersCount({ where });
+    const orders = await marketplaceClient.getOrders({
+      where,
+      offset: (page - 1) * limit,
+      count: limit,
+      // @ts-expect-error - sort by db_write_timestamp
+      sort: { db_write_timestamp: 'desc' },
+    });
+
+    return Response.json({ orders, total }, { status: 200 });
+  } catch {
+    return Response.json({ error: 'Error on getting orders' }, { status: 500 });
+  }
+}
