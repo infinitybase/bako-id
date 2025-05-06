e23eb2e130fb12109dcbd2768000ce56ec0e8ebc
vitor <pereiravitor1218@gmail.com>
2025-05-06 10:36:57 -0300

    feat: refactor order hooks to use custom mutation with polling and streamline query handling

--- apps/ui/src/hooks/marketplace/useCreateOrder.ts
+++ apps/ui/src/hooks/marketplace/useCreateOrder.ts
@@ -1,76 +1,85 @@
 import { useProfile } from '@/modules/profile/hooks/useProfile';
-import {
-  OrderStatus,
-  type Order as OrderWithMetadata,
-} from '@/types/marketplace';
+import type { Order as OrderWithMetadata } from '@/types/marketplace';
 import { MarketplaceQueryKeys } from '@/utils/constants';
-import { getOrderMetadata, type OrderResponse } from '@/utils/getOrderMetadata';
 import type { PaginationResult } from '@/utils/pagination';
 import type { Order } from '@bako-id/marketplace';
-import { useAccount } from '@fuels/react';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { type InfiniteData, useQueryClient } from '@tanstack/react-query';
 import { useSearch } from '@tanstack/react-router';
 import { useChainId } from '../useChainId';
+import { useMutationWithPolling } from '../useMutationWithPolling';
 import { useMarketplace } from './useMarketplace';
 
 export const useCreateOrder = () => {
   const marketplaceContract = useMarketplace();
   const queryClient = useQueryClient();
-  const { account } = useAccount();
   const { chainId } = useChainId();
   const { domain } = useProfile();
-  const { page } = useSearch({ strict: false });
+  const { page: pageUrl, search } = useSearch({ strict: false });
 
   const address = domain?.Address?.bits || domain?.ContractId?.bits;
+  const page = Number(pageUrl || 1);
 
   const {
     mutate: createOrder,
     mutateAsync: createOrderAsync,
     ...rest
-  } = useMutation({
+  } = useMutationWithPolling({
     mutationFn: async (order: Order) => {
       const marketplace = await marketplaceContract;
       return await marketplace.createOrder(order);
     },
-    onSuccess: async (response, payload) => {
-      const _page = page ?? 1;
-      const previousOrders = queryClient.getQueryData<PaginationResult<Order>>([
-        MarketplaceQueryKeys.ORDERS,
-        address,
-        _page,
-        chainId,
-      ]);
-
-      const order: OrderResponse = {
-        __typename: 'Order',
-        amount: '1',
-        asset: payload.sellAsset,
-        itemAsset: payload.itemAsset,
-        itemPrice: payload.sellPrice.toString(),
-        seller: account!,
-        status: OrderStatus.CREATED,
-        id: String(response.orderId),
-      };
-
-      const newOrder = await getOrderMetadata(order, chainId);
-
-      const updatedOrders = [newOrder, ...(previousOrders?.data ?? [])].slice(
-        0,
-        previousOrders?.limit
-      );
-
-      queryClient.setQueriesData<PaginationResult<OrderWithMetadata>>(
-        { queryKey: [MarketplaceQueryKeys.ORDERS], exact: false },
-        (old) =>
-          ({
-            ...old,
-            data: updatedOrders,
-          }) as PaginationResult<OrderWithMetadata>
-      );
-
-      // refetch the nfts list
-      queryClient.invalidateQueries({ queryKey: ['nfts'] });
+    mutationOpts: {
+      onSuccess: async () => {
+        await queryClient.invalidateQueries({ queryKey: ['nfts'] });
+      },
     },
+    pollConfigs: [
+      {
+        getQueryKey: () => [
+          MarketplaceQueryKeys.ORDERS,
+          address,
+          page,
+          chainId,
+        ],
+        isDataReady: (
+          data: PaginationResult<OrderWithMetadata> | undefined,
+          _,
+          { orderId }
+        ) => {
+          // prevents the infinite loop when the order is not found in the cache
+          if (!data) return true;
+          console.log('profile', orderId, data);
+          const order = data.data.find((order) => order.id === orderId);
+          console.log('order', order);
+
+          return !!order;
+        },
+      },
+      {
+        getQueryKey: () => [
+          MarketplaceQueryKeys.ALL_ORDERS,
+          chainId,
+          search ?? '',
+        ],
+        // @ts-expect-error - TODO: fix this type
+        isDataReady: (
+          data: InfiniteData<PaginationResult<OrderWithMetadata>> | undefined,
+          _,
+          { orderId }
+        ) => {
+          // prevents the infinite loop when the order is not found in the cache
+          if (!data) return true;
+
+          console.log('all', orderId, data);
+          const order = data.pages
+            .flatMap((page) => page.data)
+            .find((order) => order.id === orderId);
+          console.log('order', order);
+
+          return !!order;
+        },
+      },
+    ],
   });
 
   return { createOrder, createOrderAsync, ...rest };
--- apps/ui/src/hooks/marketplace/useExecuteOrder.ts
+++ apps/ui/src/hooks/marketplace/useExecuteOrder.ts
@@ -1,20 +1,20 @@
 import { useProfile } from '@/modules/profile/hooks/useProfile';
-import { marketplaceService } from '@/services/marketplace';
 import type { Order } from '@/types/marketplace';
 import { MarketplaceQueryKeys } from '@/utils/constants';
-import { getOrderMetadata } from '@/utils/getOrderMetadata';
 import type { PaginationResult } from '@/utils/pagination';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useQueryClient, type InfiniteData } from '@tanstack/react-query';
 import { useSearch } from '@tanstack/react-router';
 import { useChainId } from '../useChainId';
+import { useMutationWithPolling } from '../useMutationWithPolling';
 import { useMarketplace } from './useMarketplace';
 
 export const useExecuteOrder = () => {
   const marketplaceContract = useMarketplace();
-  const queryClient = useQueryClient();
   const { domain } = useProfile();
+  const queryClient = useQueryClient();
   const { chainId } = useChainId();
-  const { page } = useSearch({ strict: false });
+  const { page: pageUrl, search } = useSearch({ strict: false });
+  const page = Number(pageUrl || 1);
 
   const address = domain?.Address?.bits || domain?.ContractId?.bits;
 
@@ -22,57 +22,51 @@ export const useExecuteOrder = () => {
     mutate: executeOrder,
     mutateAsync: executeOrderAsync,
     ...rest
-  } = useMutation({
+  } = useMutationWithPolling({
     mutationFn: async (orderId: string) => {
       const marketplace = await marketplaceContract;
       return await marketplace.executeOrder(orderId);
     },
-    onSuccess: async (_, orderId) => {
-      const _page = page ?? 1;
-      const previousOrders = queryClient.getQueryData<PaginationResult<Order>>([
-        MarketplaceQueryKeys.ORDERS,
-        address,
-        _page,
-        chainId,
-      ]) as PaginationResult<Order>;
-
-      const updatedOrders =
-        previousOrders?.data.filter((order) => order.id !== orderId) ?? [];
-      const hasNextPage = previousOrders?.hasNextPage;
-
-      if (hasNextPage) {
-        const nextPage = previousOrders.page + 1;
-        const nextPageOrders = queryClient.getQueryData<
-          PaginationResult<Order>
-        >([MarketplaceQueryKeys.ORDERS, address, nextPage, chainId]);
-
-        if (nextPageOrders) {
-          const firstOrder = nextPageOrders.data[0];
-          updatedOrders.push(firstOrder);
-        }
-
-        const nextOrder = await marketplaceService.getOrdersByAccount({
-          account: address!,
-          page: nextPage,
-        });
-
-        if (nextOrder.orders.length > 0) {
-          const order = nextOrder.orders[0];
-          const orderWithMetadata = await getOrderMetadata(order, chainId);
-          updatedOrders.push(orderWithMetadata);
-        }
-      }
-
-      const paginatedOrders = {
-        ...previousOrders,
-        data: updatedOrders,
-      };
-
-      queryClient.setQueryData<PaginationResult<Order>>(
-        [MarketplaceQueryKeys.ORDERS, address, page, chainId],
-        paginatedOrders
-      );
+    mutationOpts: {
+      onSuccess: async () => {
+        await queryClient.invalidateQueries({ queryKey: ['nfts'] });
+      },
     },
+    pollConfigs: [
+      {
+        getQueryKey: () => [
+          MarketplaceQueryKeys.ORDERS,
+          address,
+          page,
+          chainId,
+        ],
+        isDataReady: (data: PaginationResult<Order> | undefined, orderId) => {
+          if (!data) return true;
+
+          const order = data.data.find((order) => order.id === orderId);
+
+          return !!order;
+        },
+      },
+      {
+        getQueryKey: () => [
+          MarketplaceQueryKeys.ALL_ORDERS,
+          chainId,
+          search || '', // -> search
+        ],
+        // @ts-expect-error - TODO: Fix this type
+        isDataReady: (
+          data: InfiniteData<PaginationResult<Order>> | undefined,
+          orderId
+        ) => {
+          if (!data) return true;
+
+          const orders = data.pages.flatMap((page) => page.data);
+
+          return !orders.find((order) => order.id === orderId);
+        },
+      },
+    ],
   });
 
   return { executeOrder, executeOrderAsync, ...rest };
