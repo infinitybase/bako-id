17d97d12d77ad45d48a30cf36b8f2c250c74777a
vitor <pereiravitor1218@gmail.com>
2025-05-13 20:28:43 -0300

    feat(profile): add ProfileHandleBanner component and integrate it into ProfileWithoutHandler

--- apps/ui/src/modules/profile/components/profileWithoutHandler.tsx
+++ apps/ui/src/modules/profile/components/profileWithoutHandler.tsx
@@ -1,9 +1,9 @@
 import { useChainId } from '@/hooks/useChainId';
-import { Stack } from '@chakra-ui/react';
+import { Container, Stack } from '@chakra-ui/react';
 import { useParams } from '@tanstack/react-router';
 import { NftCollections } from './nft/NftCollections';
 import { NftListForSale } from './nft/NftListForSale';
-import ProfileContainer from './profileContainer';
+import ProfileHandleBanner from './profileHandleBanner';
 
 export default function ProfileWithoutHandler() {
   const { chainId } = useChainId();
@@ -12,12 +12,27 @@ export default function ProfileWithoutHandler() {
   const address = domainParam.toLowerCase();
 
   return (
-    <ProfileContainer>
-      <Stack w="full" gap={6}>
+    <Container
+      maxWidth="container.xl"
+      pt={8}
+      pb={20}
+      overflowY="scroll"
+      display="flex"
+      flexDirection="column"
+      sx={{
+        '&::-webkit-scrollbar': {
+          width: '0px',
+        },
+      }}
+      maxH="100vh"
+    >
+      <Stack w="full" height="full" flex={1} gap={6} pb={10}>
+        <ProfileHandleBanner />
+
         <NftListForSale address={address} />
 
         <NftCollections resolver={address} chainId={chainId} />
       </Stack>
-    </ProfileContainer>
+    </Container>
   );
 }
