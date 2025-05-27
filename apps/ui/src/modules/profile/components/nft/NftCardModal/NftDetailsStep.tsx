115cd8f4929520a11cbf3ec328acedf52c888c32
vitor <pereiravitor1218@gmail.com>
2025-05-26 21:04:34 -0300

    feat(nft-card): update sale form

---
+++ apps/ui/src/modules/profile/components/nft/NftCardModal/NftDetailsStep.tsx
@@ -0,0 +1,108 @@
+import { LightIcon } from '@/components';
+import { BTCIcon } from '@/components/icons/btcicon';
+import { ContractIcon } from '@/components/icons/contracticon';
+import { BAKO_CONTRACTS_IDS } from '@/utils/constants';
+import {
+  Box,
+  Button,
+  Grid,
+  GridItem,
+  Heading,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
+import { useMemo } from 'react';
+import { NftListMetadata } from '../NftListMetadata';
+import { NftMetadataBlock } from '../NftMetadataBlock';
+
+export default function NftDetailStep({
+  nftName,
+  contractId,
+  assetId,
+  metadata,
+  collection,
+  isOwner,
+  onSellClick,
+}: {
+  nftName: React.ReactNode;
+  isOwner: boolean;
+  contractId?: string;
+  assetId?: string;
+  metadata?: Record<string, string>;
+  collection?: string;
+  onSellClick: () => void;
+}) {
+  const metadataArray = useMemo(() => {
+    return Object.entries(metadata ?? {}).map(([key, value]) => ({
+      label: key,
+      value,
+    }));
+  }, [metadata]);
+
+  const isBakoIdNft = useMemo(
+    () => BAKO_CONTRACTS_IDS.includes(contractId!),
+    [contractId]
+  );
+
+  return (
+    <Stack
+      w="full"
+      overflowY={{
+        base: 'unset',
+        md: 'scroll',
+      }}
+      style={{ scrollbarWidth: 'none' }}
+      maxH={{ md: '480px' }}
+    >
+      <Heading fontSize="xl">{nftName}</Heading>
+      <Stack spacing={6} flex={1} mt={6} maxH="full">
+        <Box>
+          <Heading fontSize="md">Description</Heading>
+          <Text mt={3} fontSize="sm" color="section.500" wordBreak="break-all">
+            {metadata?.description ?? 'Description not provided.'}
+          </Text>
+        </Box>
+
+        <Grid templateColumns="repeat(2, 1fr)" gap={4}>
+          {assetId && (
+            <GridItem>
+              <NftMetadataBlock
+                icon={<BTCIcon />}
+                value={assetId}
+                title="Asset ID"
+                isCopy
+              />
+            </GridItem>
+          )}
+
+          {collection && (
+            <GridItem>
+              <NftMetadataBlock
+                value={collection}
+                title="Creator"
+                icon={<LightIcon />}
+              />
+            </GridItem>
+          )}
+
+          <GridItem>
+            <NftMetadataBlock
+              icon={<ContractIcon />}
+              value={contractId!}
+              title="Contract Address"
+              isCopy
+            />
+          </GridItem>
+        </Grid>
+
+        {isOwner && !isBakoIdNft && (
+          <Button variant="primary" w="full" onClick={onSellClick} py={4}>
+            List
+          </Button>
+        )}
+
+        <NftListMetadata metadata={metadataArray} />
+      </Stack>
+    </Stack>
+  );
+}
---
+++ apps/ui/src/modules/profile/components/nft/NftCardModal/index.tsx
@@ -0,0 +1,89 @@
+import { useResolverName } from '@/hooks';
+import type { Asset } from '@/types/marketplace';
+import { parseURI } from '@/utils/formatter';
+import { useWallet } from '@fuels/react';
+import { ZeroBytes32 } from 'fuels';
+import { useCallback, useState } from 'react';
+import { NftModal } from '../modal';
+import NftDetailStep from './NftDetailsStep';
+import NftFormStep from './NftFormStep';
+
+interface NftCardModalProps {
+  assetId: string;
+  contractId?: string;
+  nftName: React.ReactNode;
+  metadata?: Record<string, string>;
+  image: string;
+  isOpen: boolean;
+  onClose: () => void;
+  assets: Asset[];
+  isOwner: boolean;
+  collection?: string;
+}
+
+export const NftCardModal = ({
+  assetId,
+  nftName,
+  contractId,
+  metadata,
+  image,
+  isOpen,
+  onClose,
+  assets,
+  isOwner,
+  collection,
+}: NftCardModalProps) => {
+  const [step, setStep] = useState(0);
+  const { wallet } = useWallet();
+  const { data } = useResolverName(wallet?.address.b256Address || ZeroBytes32);
+
+  const handleChangeStepToSell = useCallback(() => {
+    setStep(1);
+  }, []);
+
+  const handleChangeStepToDetails = useCallback(() => {
+    setStep(0);
+  }, []);
+
+  return (
+    <NftModal.Root onClose={onClose} isOpen={isOpen}>
+      <NftModal.Content
+        flexDirection={{
+          base: 'column',
+          md: 'row',
+        }}
+        maxH="540px"
+        overflowY={{
+          base: 'scroll',
+          md: 'hidden',
+        }}
+      >
+        <NftModal.Image w="full" src={parseURI(image)} alt="NFT image" />
+        {step === 0 && (
+          <NftDetailStep
+            isOwner={isOwner}
+            nftName={nftName}
+            contractId={contractId}
+            assetId={assetId}
+            metadata={metadata}
+            collection={collection}
+            onSellClick={handleChangeStepToSell}
+          />
+        )}
+
+        {step === 1 && (
+          <NftFormStep
+            assetId={assetId}
+            name={nftName}
+            onCancel={handleChangeStepToDetails}
+            onClose={onClose}
+            userWithHandle={!!data}
+            assets={assets}
+          />
+        )}
+
+        <NftModal.CloseIcon onClose={onClose} />
+      </NftModal.Content>
+    </NftModal.Root>
+  );
+};
