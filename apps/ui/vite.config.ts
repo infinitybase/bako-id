61e0cb19e250699bf752e431af4d3133dd360be7
luisburigo <gugaba29@gmail.com>
2025-07-15 14:45:31 -0300

    chore(marketplace): improve router tree resolver

--- apps/ui/vite.config.ts
+++ apps/ui/vite.config.ts
@@ -2,8 +2,11 @@ import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+console.log(`Vercel ENV: ${process.env.VERCEL_ENV}`);
+
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
+  mode: process.env.VERCEL_ENV,
   resolve: {
     alias: {
       '@': '/src',
