7db12a36571ab17cb72799a0bd3e5194c341092a
Rafael <rafael-dev@hotmail.com>
2025-08-18 14:18:42 -0300

    fix: couldnt open the nft sale card in the bako id homepage

--- apps/ui/src/modules/profile/components/profileWithoutHandler.tsx
+++ apps/ui/src/modules/profile/components/profileWithoutHandler.tsx
@@ -45,6 +45,7 @@ export default function ProfileWithoutHandler({
           orders={orders}
           isLoadingOrders={isLoadingOrders}
           paginationInfos={paginationInfos}
+          isProfilePage={true}
         />
 
         <NftCollections resolver={address} />
