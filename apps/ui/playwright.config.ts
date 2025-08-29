dee947c2b62163cb9cbcd5872550376a9103eb57
Caio Balieiro <caiobalieiro676@gmail.com>
2025-08-29 06:42:02 -0300

    chore: remove forbidOnly option from Playwright config

--- apps/ui/playwright.config.ts
+++ apps/ui/playwright.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: 1,
   timeout: 180000,
