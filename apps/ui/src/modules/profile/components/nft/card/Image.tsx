d28c823d04dc237062c6deb93f434f56807b84c8
Rafael <rafael-dev@hotmail.com>
2025-09-15 18:06:46 -0300

    chore: pass image props directly without fallback src

--- apps/ui/src/modules/profile/components/nft/card/Image.tsx
+++ apps/ui/src/modules/profile/components/nft/card/Image.tsx
@@ -4,8 +4,6 @@ import type { ImageProps } from '@chakra-ui/react';
 export const Image = ({ boxSize = { xl: '160px ' }, ...props }: ImageProps) => {
   const { src, alt, ...rest } = props;
 
-  console.log('src', src);
-
   return (
     <ImageLoader
       src={src!}
