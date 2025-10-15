cede9defa34bbdbb7ade16379e2fdd9f9fd222c1
luisburigo <gugaba29@gmail.com>
2025-10-15 12:32:28 -0300

    chore(indexer): update envio

--- apps/server/app/api/[network]/name/[addr]/route.ts
+++ apps/server/app/api/[network]/name/[addr]/route.ts
@@ -8,10 +8,13 @@ export async function GET(req: NextRequest) {
     .filter((a) => !!a);
 
   try {
+    console.log(Address.fromString(addr).toB256());
     const { data } = await graphqlClient.sdk.name({
       address: Address.fromString(addr).toB256(),
       network: network.toUpperCase(),
     });
+
+    console.log({ data });
 
     const record = data.AddressResolver.at(0);
     return NextResponse.json({ name: record?.name ?? null });
